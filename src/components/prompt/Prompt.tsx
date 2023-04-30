f2f1a3eec093cf7811dcd44d26794f0a1ad7f1be
kasia.dziewicka <kasia.dziewicka@faculty.ai>
2023-04-30 11:01:54 +0100

    Display random prompts on the screen

--- src/components/prompt/Prompt.tsx
+++ src/components/prompt/Prompt.tsx
@@ -1,6 +1,29 @@
-import React from "react";
+import { AirtableBase } from "airtable/lib/airtable_base";
+import { useEffect, useState } from "react";
 
-const Prompt = ({ prompt }: { prompt: string }) => {
+const Prompt = ({ base }: { base: AirtableBase }) => {
+  const [prompt, setPrompt] = useState("");
+
+  useEffect(() => {
+    base("Table 1")
+      .select({
+        view: "Grid view",
+      })
+      .all((err, records) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        if (records && records.length) {
+          console.log(records);
+          const randomRecord =
+            records[Math.floor(Math.random() * records.length)]._rawJson.fields
+              .Prompt;
+          setPrompt(randomRecord);
+        }
+      });
+  }, [base]);
+
   return <p>{prompt}</p>;
 };
 
