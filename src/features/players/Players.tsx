7aab7974a287fe14662434dec175f068de229fe7
kasia.dziewicka <kasia.dziewicka@faculty.ai>
2023-04-30 11:02:03 +0100

    Create players page

--- src/features/players/Players.tsx
+++ src/features/players/Players.tsx
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
-import "./App.css";
 
 const Players = () => {
-  const [playerName, setPlayerName] = useState("");
+  const theGroup = [
+    "Rita",
+    "Tomim",
+    "Kasia",
+    "Kalid",
+    "Bea",
+    "Zoobi",
+    "Ad",
+    "Cam",
+    "Miro",
+    "Tid",
+    "Alice",
+    "Elliot",
+    "Imu",
+  ];
+  const [newPlayerName, setNewPlayerName] = useState("");
   const [playersList, setPlayersList] = useState<string[]>([]);
 
   return (
@@ -10,18 +24,38 @@ const Players = () => {
       <header className="App-header">
         <p>Welcome to Neverland Picolo</p>
         <p>Who's playing?</p>
-        <p>{playersList[0]}</p>
-        <form>
+        {theGroup.map((name) => (
+          <button
+            key={`add-${name}`}
+            onClick={() => setPlayersList([...playersList, name])}
+          >
+            {name}
+          </button>
+        ))}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setPlayersList([...playersList, newPlayerName]);
+          }}
+        >
           <input
             placeholder="Type the name of a player..."
-            // onChange={(e: string) => setPlayerName(e)}
+            onChange={(e) => setNewPlayerName(e.target.value)}
           />
-          <input
-            type="submit"
-            value="add"
-            onClick={() => setPlayersList([playerName])}
-          />
+          <input type="submit" value="add" />
         </form>
+        <button onClick={() => setPlayersList([])}>Clear</button>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "20px",
+          }}
+        >
+          {playersList.map((player) => (
+            <p>{player}</p>
+          ))}
+        </div>
       </header>
     </div>
   );
