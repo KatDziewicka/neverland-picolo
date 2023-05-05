df42398c6e44aad118baed07b895641dda33dbc5
kasia.dziewicka <kasia.dziewicka@faculty.ai>
2023-05-05 10:07:13 +0100

    Add play/go back buttons and re-render of prompts

--- src/components/prompt/Prompt.tsx
+++ src/components/prompt/Prompt.tsx
@@ -1,8 +1,12 @@
 import { AirtableBase } from "airtable/lib/airtable_base";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Prompt = ({ base }: { base: AirtableBase }) => {
   const [prompt, setPrompt] = useState("");
+  const [componentState, updateComponentState] = useState<Object>();
+  // TODO: see if this useCallback is necessary?
+  const forceUpdate = useCallback(() => updateComponentState({}), []);
 
   useEffect(() => {
     base("Table 1")
@@ -18,13 +22,21 @@ const Prompt = ({ base }: { base: AirtableBase }) => {
           console.log(records);
           const randomRecord =
             records[Math.floor(Math.random() * records.length)]._rawJson.fields
-              .Prompt;
+              .prompt;
           setPrompt(randomRecord);
         }
       });
-  }, [base]);
+  }, [base, componentState]);
 
-  return <p>{prompt}</p>;
+  return (
+    <div className="App-header">
+      <p>{prompt}</p>
+      <button onClick={forceUpdate}>NEXT</button>
+      <Link to="/home">
+        <button>GO BACK</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Prompt;
--- src/features/players/Players.tsx
+++ src/features/players/Players.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Players = () => {
   const theGroup = [
@@ -20,43 +21,48 @@ const Players = () => {
   const [playersList, setPlayersList] = useState<string[]>([]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Welcome to Neverland Picolo</p>
-        <p>Who's playing?</p>
-        {theGroup.map((name) => (
-          <button
-            key={`add-${name}`}
-            onClick={() => setPlayersList([...playersList, name])}
-          >
-            {name}
-          </button>
+    // TODO this shouldn't be called App-header
+    <div className="App-header">
+      <p>Welcome to Neverland Picolo</p>
+      <p>Who's playing?</p>
+      {theGroup.map(
+        (name) =>
+          !playersList.includes(name) && (
+            <button
+              key={`add-${name}`}
+              onClick={() => setPlayersList([...playersList, name])}
+            >
+              {name}
+            </button>
+          )
+      )}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setPlayersList([...playersList, newPlayerName]);
+        }}
+      >
+        <input
+          placeholder="Type the name of a player..."
+          onChange={(e) => setNewPlayerName(e.target.value)}
+        />
+        <input type="submit" value="add" />
+      </form>
+      <button onClick={() => setPlayersList([])}>Clear</button>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "20px",
+        }}
+      >
+        {playersList.map((player) => (
+          <p>{player}</p>
         ))}
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            setPlayersList([...playersList, newPlayerName]);
-          }}
-        >
-          <input
-            placeholder="Type the name of a player..."
-            onChange={(e) => setNewPlayerName(e.target.value)}
-          />
-          <input type="submit" value="add" />
-        </form>
-        <button onClick={() => setPlayersList([])}>Clear</button>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            gap: "20px",
-          }}
-        >
-          {playersList.map((player) => (
-            <p>{player}</p>
-          ))}
-        </div>
-      </header>
+      </div>
+      <Link to="/play">
+        <button>PLAY</button>
+      </Link>
     </div>
   );
 };
